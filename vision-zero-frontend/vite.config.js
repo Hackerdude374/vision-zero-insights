804389e7d133c9850b9a7f545fd103a02c503e4a
Robert Le <robertle982@gmail.com>
2025-06-23 13:43:22 -0400

    updated vite and tailwind config js frontend has markers

--- vision-zero-frontend/vite.config.js
+++ vision-zero-frontend/vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://vision-zero-insights.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+      '/api': 'https://vision-zero-insights.onrender.com'
+    }
+  }
+})
