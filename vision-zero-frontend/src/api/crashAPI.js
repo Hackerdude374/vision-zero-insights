978a8650a722a4d85f6f8b798b579199e8078d2a
Robert Le <robertle982@gmail.com>
2025-06-23 15:36:22 -0400

    working frontend api crash data

--- vision-zero-frontend/src/api/crashAPI.js
+++ vision-zero-frontend/src/api/crashAPI.js
@@ -1,8 +1,9 @@
 // src/api/crashAPI.js
 export async function fetchCrashData() {
-  const res = await fetch("/api/crashes");
+  const res = await fetch("https://vision-zero-insights.onrender.com/api/crashes");
   if (!res.ok) {
-    throw new Error("Failed to fetch crash data");
+    throw new Error("Failed to fetch crash data!!!!!!!!!!!");
   }
+  console.log ("HELLO!!!")
   return res.json();
 }
